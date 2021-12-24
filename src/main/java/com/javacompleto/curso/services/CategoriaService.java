c1b44af4651a0b51f558f4058bac711a905b69b8
Willams Souza <willamsbernardo@hotmail.com>
2021-12-24 14:27:12 -0300

    Criando Repository e Service para Categoria

---
+++ src/main/java/com/javacompleto/curso/services/CategoriaService.java
@@ -0,0 +1,21 @@
+package com.javacompleto.curso.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.javacompleto.curso.domain.Categoria;
+import com.javacompleto.curso.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
+	public Categoria findById(Integer id) {
+		Optional<Categoria> obj = categoriaRepository.findById(id);
+		return obj.orElse(null);
+	}
+}
